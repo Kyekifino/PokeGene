2d36f4f2d4ae55e130d2672de2158489de08fd72
Thomas Dickerson <tadicke3@ncsu.edu>
2018-08-31 17:13:47 -0400

    Added names for Water/Grass/Ice Pokemon. #10

--- SpeciesNames.py
+++ SpeciesNames.py
@@ -1,10 +1,10 @@
 # Dictionary of name options. Each type contains a list of 20 possible prefixes in a Pokemon's name.
 prefix_dict = {"Normal": ["Me", "Pup", "Kanga", "Nor", "Ante", "Chip", "Fur", "Sap", "Ursa", "Neko", "Hound", "Pig", "Beige", "Bun", "Bov", "Lil", "Great", "Bear", "Ele", "Pure"],
              "Fire": ["Fla", "Infer", "Confla", "Em", "Pyro", "Lumin", "Tin", "Kin", "Igni", "Ex", "Char", "Oxi", "Incan", "Com", "Bon", "Camp", "Tor", "Warm", "Fieri", "Swel"],
-             "Water": [],
-             "Electric": [],
-             "Grass": [],
-             "Ice": [],
+             "Water": ["Wa", "Wave", "Hy", "Spla", "Aqua", "Dol", "Ocea", "An", "Su", "Sal", "Sea", "Noz", "Tor", "Tur", "Lob", "Land", "Or", "Atlan", "Ty", "Tidal"],
+             "Electric": ["Vol", "Elec", "Cur", "Di", "Mo", "Mag", "Thun", "Light", "Max", "Resis", "Cir", "Amp", "Ampli", "Over", "Tran", "Pro", "Switch", "Bread", "Ac", "Pow"],
+             "Grass": ["Synth", "For", "Tree", "Coco", "Flow", "Vege", "Wood", "Plant", "Chloro", "Leaf", "Bam", "Pump", "Musk", "Ever", "Palm", "Vine", "Bark", "Pet", "Brush", "Jun"],
+             "Ice": ["Bliz", "Glac", "Fro", "Hail", "Ava", "Ice", "Frost", "Snow", "Arc", "Pol", "Aur", "Nar", "Pen", "Cryo", "Dry", "Icy", "Jack", "Win", "Sub", "Refridg"],
              "Fighting": [],
              "Poison": [],
              "Ground": [],
@@ -20,12 +20,12 @@ prefix_dict = {"Normal": ["Me", "Pup", "Kanga", "Nor", "Ante", "Chip", "Fur", "S
              }
 
 # Dictionary of name options. Each type contains a list of 20 possible suffixes in a Pokemon's name.
-prefix_dict = {"Normal": ["ow", "py", "roo", "mal", "lope", "munk", "coat", "ian", "major", "mata", "dog, "let", "tint", "ny", "ine", "peep", "dane", "claw", "phant", "clear"],
+suffix_dict = {"Normal": ["ow", "py", "roo", "mal", "lope", "munk", "coat", "ian", "major", "mata", "dog, "let", "tint", "ny", "ine", "peep", "dane", "claw", "phant", "clear"],
              "Fire": ["ming", "no", "gration", "ber", "maniac", "ous", "der", "dle", "tion", "plode", "coal", "dation", "descent", "bust", "fire", "flame", "rid", "burn", "heat", "ter"],
-             "Water": [],
-             "Electric": [],
-             "Grass": [],
-             "Ice": [],
+             "Water": ["ter", "pool", "dro", "shing", "tic", "phin", "nic", "chor", "shi", "mon", "horse", "zle", "toise", "tle", "ster", "lubber", "ca", "tis", "phoon", "wave"],
+             "Electric": ["tage", "tric", "rent", "ode", "tor", "net", "der", "ning", "well", "tor", "cuit", "ere", "fier", "flow", "sister", "tron", "plate", "board", "decee", "er"],
+             "Grass": ["esis", "est", "top", "nut", "er", "table", "works", "life", "phyll", "let", "boo", "kin", "melon", "green", "tree", "whip", "wood", "al", "weed", "gle"],
+             "Ice": ["zard", "ier", "zen", "fall", "lanche", "berg", "bite", "man", "tic", "ar", "ora", "whal", "guin", "genic", "ice", "cle", "frost", "ter", "zero", "erator"],
              "Fighting": [],
              "Poison": [],
              "Ground": [],
